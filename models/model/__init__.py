85df1829f090cd32cdcc8ddce2a6c86300c53757
anpro948 <anpro948@gmail.com>
2023-12-05 02:29:08 +0700

    ''

---
+++ models/model/__init__.py
@@ -0,0 +1,1 @@
+from .smtopic import SMTopic
---
+++ models/model/backend/_utils.py
@@ -0,0 +1,18 @@
+from ._base import BaseEmbedder
+from ._sentencetransformers import SentenceTransformerBackend
+from ._sentenceTFIDF import TFIDF_SenEmbed
+from ._sentenceUSE import USE_SenEmbed
+
+
+def select_backend(embedding_model):
+  
+    if isinstance(embedding_model, str):
+        if "tfidf" in embedding_model:
+            return TFIDF_SenEmbed
+        elif "use" in embedding_model:
+            return USE_SenEmbed
+        else:
+            return SentenceTransformerBackend(embedding_model)
+    else:
+        raise ValueError("The embedding model must be a string")
+    
