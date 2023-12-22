cf1fdd04239cb12db20f0b91cd28c0c43603f323
anpro948 <39051090+anpro948@users.noreply.github.com>
2023-12-22 11:34:50 +0700

    ""

--- models/model/backend/_utils.py
+++ models/model/backend/_utils.py
@@ -2,7 +2,7 @@ from ._base import BaseEmbedder
 from ._sentencetransformers import SentenceTransformerBackend
 from ._sentenceTFIDF import TFIDF_SenEmbed
 from ._sentenceUSE import USE_SenEmbed
-
+from ._sentencew2v import Word2Vec_SenEmbed
 
 def select_backend(embedding_model):
   
@@ -11,7 +11,8 @@ def select_backend(embedding_model):
             return TFIDF_SenEmbed()
         elif "use" in embedding_model:
             return USE_SenEmbed()
-        
+        elif 'word2vec' in embedding_model:
+            return Word2Vec_SenEmbed()
         else:
             return SentenceTransformerBackend(embedding_model)
     else:
